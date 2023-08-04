7e16752a4b959216863028fde4592f945cfdbe2f
Roberto <robertojhdzs@gmail.com>
2023-08-04 14:27:00 -0600

    Revert "DEsarollo de MiniSpeeds todo lo grafico"
    This reverts commit cc05fa052ca4ecb1db79202b63a7929d7457f113.

--- js/test.js
+++
@@ -1,8 +0,0 @@
-function noche(){
-   const element = document.getElementById("claro");
-   const element1 = document.getElementById("claro1");
-   element.classList.toggle('modo__noche');
-   element1.classList.toggle('modo__noche');
-}
-
-// document.getElementsByClassName("modo__noche__boton").innerHTML = "lol"
