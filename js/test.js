cc05fa052ca4ecb1db79202b63a7929d7457f113
Roberto <robertojhdzs@gmail.com>
2023-08-04 13:10:23 -0600

    DEsarollo de MiniSpeeds todo lo grafico

---
+++ js/test.js
@@ -0,0 +1,8 @@
+function noche(){
+   const element = document.getElementById("claro");
+   const element1 = document.getElementById("claro1");
+   element.classList.toggle('modo__noche');
+   element1.classList.toggle('modo__noche');
+}
+
+// document.getElementsByClassName("modo__noche__boton").innerHTML = "lol"
